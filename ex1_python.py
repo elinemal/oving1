0966e196e50e66080339345719d86148160c4df0
elinemal <e_malde@hotmail.com>
2017-01-11 11:10:13 +0100

    Update ex1_python.py

--- ex1_python.py
+++ ex1_python.py
@@ -4,9 +4,31 @@ from threading import Thread
 
 i = 0
 
-def adding():
+    global i
+
+def add(): #thread1
+  for x in range(0,1000000):
+      global i
+      i+=1
   
-def sub():
+def sub(): #thread2
+  for x in range(0,1000000):
+      global i
+      i-=1
   
 def main():
-  
+    thread1 = Thread(target = add, args = (),)
+    thread1.start()
+    
+    thread2 = Thread(target = sub, args = (),)
+    thread2.start()
+    
+    #thread1.join(thread2)
+    print(i)
+
+main()
+
+
+# Potentially useful thing:
+#   In Python you "import" a global variable, instead of "export"ing it when you declare it
+#   (This is probably an effort to make you feel bad about typing the word "global")
